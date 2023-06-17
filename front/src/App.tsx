35a49bf60ecf72804263cf62516d905b16730a3f
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-06-17 11:59:30 -0300

    Front setup

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom"
+import { RoutesApp } from "./routes/indext"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ front/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+
+  theme: {
+    extend: {
+      colors: {
+        '-primary': '#348789',
+        '-secondary': '#001489',
+        '-secondary-50': '#3e53cf',
+
+        '--gray-50': '#DDDFEB',
+        '--gray-100': '#97909050',
+        '--gray-200': '#535653',
+        '--gray-300': '#7c888388',
+
+        '--white': '#fff',
+        '--black': '#272830',
+      },
+
+      backgroundImage: {
+        'login': 'url(./src/assets/background-login.png)'
+      }
+    },
+  },
+
+  plugins: [],
+}
+
