95a04ad267cfc5c0db94cfe2e30f354001786ce1
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-06-18 21:05:12 -0300

    Allow to include icon into input

--- front/src/components/inputTransparent/index.tsx
+++ front/src/components/inputTransparent/index.tsx
@@ -2,10 +2,14 @@
 interface Props{
   placeholder: string;
   type: 'email' | 'password' | 'text' | 'tel' | 'date' | 'time';
+  icon?: JSX.Element;
 }
 
-export function InputTransparent({placeholder, type}: Props){
+export function InputTransparent({placeholder, type, icon}: Props){
   return(
-    <input type={type} placeholder={placeholder} className="bg-transparent font-light text-base -text-primary px-2 py-2 rounded-2xl -border-primary border-2 focus:outline-none placeholder:-text-primary/70"/>
+    <div className="flex gap-2 bg-transparent font-light text-base -text-primary px-2 py-2 rounded-2xl -border-primary border-2 focus:outline-none placeholder:-text-primary/70">
+      {icon ? <i>{icon}</i> : <></>}
+      <input type={type} placeholder={placeholder} className="bg-transparent focus:outline-none"/>
+    </div>
   )
 }
