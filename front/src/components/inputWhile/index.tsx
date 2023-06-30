246333b6f091ad125bb164823437f88bfe431a8e
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-06-30 09:43:42 -0300

    Initial implementation of react-hook-form passing register as props

--- front/src/components/inputWhile/index.tsx
+++ front/src/components/inputWhile/index.tsx
@@ -1,24 +1,36 @@
+import React from 'react'
+import { FieldValues, UseFormRegister } from 'react-hook-form'
 
-interface Props{
-    placeholder: string;
-    type: 'email' | 'password';
-    icon: JSX.Element;
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+    placeholder: string
+    type: 'email' | 'password'
+    icon: JSX.Element
+    register?: UseFormRegister<FieldValues> | any
 }
 
-interface IconTypeProps {
-    width: number;
-    height: number;
-    color: string;
-  }
+// interface IconTypeProps {
+//     width: number
+//     height: number
+//     color: string
+// }
 
-
-export function InputWhite({placeholder, type, icon}: Props){
-    return(
+export function InputWhite({
+    placeholder,
+    type,
+    icon,
+    register,
+    ...props
+}: Props) {
+    return (
         <div className="flex items-center gap-2 bg-white rounded-md px-4 py-2 text-md text-black">
-            <i>
-                {icon}
-            </i>
-            <input type={type} placeholder={placeholder} className="focus:outline-none placeholder:text-gray-400"/>
+            <i>{icon}</i>
+            <input
+                type={type}
+                placeholder={placeholder}
+                className="focus:outline-none placeholder:text-gray-400"
+                {...register}
+                {...props}
+            />
         </div>
     )
 }
--- front/src/pages/signup/index.tsx
+++ front/src/pages/signup/index.tsx
@@ -1,13 +1,24 @@
-import { EnvelopeSimple, Key, Person } from "phosphor-react";
-import { InputWhite } from "../../components/inputWhile";
+import { EnvelopeSimple, Key, Person } from 'phosphor-react'
+import { InputWhite } from '../../components/inputWhile'
+import { useForm } from 'react-hook-form'
 
-import logo from "../../assets/logo.png";
+import logo from '../../assets/logo.png'
 
-export function SignUp(){
+export function SignUp() {
+    const { register, handleSubmit, watch } = useForm()
+
+    function handleSubmitForm() {
+        // console.log(data)
+    }
+
+    const name = watch('name')
+    const email = watch('email')
+    const password = watch('password')
+    console.log({ name, email, password })
+
     return (
         <div className="bg-signup bg-cover bg-no-repeat">
             <div className="grid min-h-screen grid-cols-2">
-
                 {/* Left */}
                 <div className="px-20 py-20 text-gray-50">
                     <header>
@@ -15,26 +26,62 @@ export function SignUp(){
                     </header>
 
                     <div className="h-full flex items-center justify-center">
-                        <img src={logo} alt="" className="w-[274px] h-[230px]"/>
+                        <img
+                            src={logo}
+                            alt=""
+                            className="w-[274px] h-[230px]"
+                        />
                     </div>
                 </div>
 
                 {/* Right */}
                 <div className="flex items-center justify-center">
                     <div className="bg-gray-50 bg-opacity-20 px-10 py-12 rounded-3xl shadow-2xl shadow-black flex flex-col">
-                        <h2 className="mb-8 px-4 text-white text-4xl font-light">Olá, seja bem vindo!</h2>
+                        <h2 className="mb-8 px-4 text-white text-4xl font-light">
+                            Olá, seja bem vindo!
+                        </h2>
 
-                        <form className="flex flex-col gap-4">
-                            <InputWhite type="email" placeholder="Insira seu nome" icon={<Person size={20}/>}/>
-                            <InputWhite type="email" placeholder="Insira seu email" icon={<EnvelopeSimple size={20}/>}/>
-                            <InputWhite type="password" placeholder="Insira sua senha" icon={<Key size={20}/>}/>
-                            <button className="mt-3 py-2 w-full bg-blue-800 text-white font-bold rounded-2xl hover:bg-blue-600 transition-colors">Cadastrar</button>
+                        <form
+                            className="flex flex-col gap-4"
+                            onSubmit={handleSubmit(handleSubmitForm)}
+                        >
+                            <InputWhite
+                                type="email"
+                                placeholder="Insira seu nome"
+                                icon={<Person size={20} />}
+                                id="name"
+                                value={name}
+                                register={{ ...register('name') }}
+                            />
+                            <InputWhite
+                                type="email"
+                                placeholder="Insira seu email"
+                                icon={<EnvelopeSimple size={20} />}
+                                id="email"
+                                value={email}
+                                register={{ ...register('email') }}
+                            />
+                            <InputWhite
+                                type="password"
+                                placeholder="Insira sua senha"
+                                icon={<Key size={20} />}
+                                id="password"
+                                value={password}
+                                register={{ ...register('password') }}
+                            />
+                            <button className="mt-3 py-2 w-full bg-blue-800 text-white font-bold rounded-2xl hover:bg-blue-600 transition-colors">
+                                Cadastrar
+                            </button>
                         </form>
 
-                        <span className="mt-4 text-gray-50 text-xs">Já tem cadastro? <a className="font-bold text-white hover:underline hover:cursor-pointer hover:text-gray-50">Voltar à Página Inicial</a></span>
+                        <span className="mt-4 text-gray-50 text-xs">
+                            Já tem cadastro?{' '}
+                            <a className="font-bold text-white hover:underline hover:cursor-pointer hover:text-gray-50">
+                                Voltar à Página Inicial
+                            </a>
+                        </span>
                     </div>
                 </div>
-
             </div>
         </div>
     )
