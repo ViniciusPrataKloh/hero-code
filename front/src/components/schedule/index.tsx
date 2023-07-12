f856944bcbf572fdaac12770873acb308afe7d35
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-07-11 23:08:57 -0300

    Format the hour of schedule

--- front/src/components/schedule/index.tsx
+++ front/src/components/schedule/index.tsx
@@ -1,16 +1,20 @@
+import { format } from 'date-fns'
 import { PencilSimple, Trash } from 'phosphor-react'
 
 interface Props {
-    hour: string
+    date: Date
     client: string
 }
 
-export function Schedule({ hour, client }: Props) {
+export function Schedule({ date, client }: Props) {
+    const hour = date.toString().split(':', 4)[1]
+    // const minutes = date.toString().split(':', 4)[2].split('.')[0]
+
     return (
         <div className="w-[492px] mt-6 flex flex-row items-center justify-between bg-white rounded-2xl shadow-lg shadow-gray-400">
             <div className="flex items-center">
                 <div className="-bg-secondary py-3 px-5 text-white rounded-l-xl">
-                    {hour}
+                    {hour}h
                 </div>
 
                 <span className=" py-3 px-5 ">{client}</span>
